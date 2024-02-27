80005f7cc82068f746c7cc4749762a99d3e348f9
azatich <azatabdirashituly@gmail.com>
2024-02-27 15:13:32 +0600

    updated redirecting

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET_KEY,
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
 }));
 
 i18next
