18de7f94b020ff45754bef0d238cbd4edba0434e
azatich <azatabdirashituly@gmail.com>
2024-02-27 14:00:42 +0600

    updated session

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(session({
-    secret: process.env.SESSION_SECRET || 'secret',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
