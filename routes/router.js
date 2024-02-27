c1742d862d964aa14be110d499a9053a32799c90
azatich <azatabdirashituly@gmail.com>
2024-02-27 15:06:32 +0600

    updated redirecting

--- routes/router.js
+++ routes/router.js
@@ -10,7 +10,7 @@ const { userController } = require('../controllers/userController')
 router.get('/', authenticateUser);
 
 router.get('/movie', authenticateUser, movieController.getMoviePage)
-router.post('/movie', movieController.getMovieData)
+router.post('/movie', authenticateUser, movieController.getMovieData)
 
 router.get('/registration', logregController.getRegistrationPage)
 router.post('/getDataFromReg', logregController.register);
@@ -34,7 +34,7 @@ router.put('/posts/edit/:id', authenticateUser, adminController.editPost);
 router.post('/comment/:id', authenticateUser, userController.comment);
 router.put('/posts/likePost/:id', authenticateUser, userController.likePost);
 
-router.get('/actor', actorController.getActorsPage)
+router.get('/actor', authenticateUser, actorController.getActorsPage)
 router.post('/actor', authenticateUser, actorController.getActorInfo)
 
 router.get('/change-lang/:lang', (req, res) => {
